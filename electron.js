c92b0a5bd15e3c89f919eb8de6d014f4f63f8fbf
HowLuks <lucas.1506.freitas@gmail.com>
2025-08-19 18:23:33 -0300

    exe

---
+++ electron.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  // Durante o desenvolvimento, carregue o servidor do Vite
+  win.loadURL('http://localhost:5173');
+
+  // Quando estiver em produção, use o build do Vite:
+  // win.loadFile(path.join(__dirname, 'dist/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
